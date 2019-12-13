d8f35cce279064068c030d97338e7c123ea28c24
Артем <art-dikov@yandex.ru>
2019-12-13 20:14:04 +0400

    H6 - fixed method sort in class ArraySort

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
+import ru.progwards.java1.lessons.arrays.ArraySort;
 import ru.progwards.java1.lessons.compare_if_cycles.CyclesGoldenFibo;
 import ru.progwards.java1.lessons.compare_if_cycles.TriangleSimpleInfo;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(CyclesGoldenFibo.containsDigit(0, 0));
-        System.out.println(CyclesGoldenFibo.containsDigit(10, 0));
-        System.out.println(CyclesGoldenFibo.containsDigit(11, 0));
-        System.out.println(CyclesGoldenFibo.containsDigit(101, 0));
+        int[] arr = {-58,-57,29,62,-77,-94,-96,55,65,78,-24,63};
+        ArraySort.sort(arr);
+        System.out.println(Arrays.toString(arr));
+
     }
 }
--- src/ru/progwards/java1/lessons/arrays/ArraySort.java
+++ src/ru/progwards/java1/lessons/arrays/ArraySort.java
@@ -3,7 +3,7 @@ package ru.progwards.java1.lessons.arrays;
 public class ArraySort {
     public static void sort(int[] a) {
         for(int i = 0; i < a.length; i++) {
-            for(int j = 0; j < a.length; j++) {
+            for(int j = i + 1; j < a.length; j++) {
                 int temp;
                 if(a[i] > a[j]) {
                     temp = a[i];
