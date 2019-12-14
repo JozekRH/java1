a12556e9bd409720dc051589af06233801d94d64
Артем <art-dikov@yandex.ru>
2019-12-15 00:44:42 +0400

    H7 - changed according with recommendations of mentor

--- src/ru/progwards/java1/lessons/bitsworld/CheckBit.java
+++ src/ru/progwards/java1/lessons/bitsworld/CheckBit.java
@@ -2,9 +2,7 @@ package ru.progwards.java1.lessons.bitsworld;
 
 public class CheckBit {
     public static int checkBit(byte value, int bitNumber) {
-        for(int i = 0; i < bitNumber; i++) {
-            value = (byte)(value >> 1);
-        }
+        value = (byte)(value >> bitNumber);
         return value & 1;
     }
 }
--- src/ru/progwards/java1/lessons/bitsworld/SumBits.java
+++ src/ru/progwards/java1/lessons/bitsworld/SumBits.java
@@ -4,9 +4,7 @@ public class SumBits {
     public static int sumBits(byte value) {
         int res = 0;
         for(int i = 0; i < 8; i++) {
-            if((value & 1) == 1){
-                res += 1;
-            }
+            res += (value & 1);
             value = (byte)(value >>> 1);
         }
         return res;
