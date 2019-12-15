5de4a63f05be867e9d72784f1c130de0ade525b7
Артем <art-dikov@yandex.ru>
2019-12-15 19:51:53 +0400

    H8 - enum FoodKind and enum AnimalKind copied from ru.progwards.java1.lessons.classes package to ru.progwards.java1.lessons.interfaces package

--- src/ru/progwards/java1/lessons/interfaces/Animal.java
+++ src/ru/progwards/java1/lessons/interfaces/Animal.java
@@ -1,8 +1,5 @@
 package ru.progwards.java1.lessons.interfaces;
 
-import ru.progwards.java1.lessons.classes.AnimalKind;
-import ru.progwards.java1.lessons.classes.FoodKind;
-
 import java.util.Objects;
 
 public class Animal implements FoodCompare, CompareWeight {
