e129879aca4d32ad76918bf30f87065aec6205a7
Артем <art-dikov@yandex.ru>
2019-12-20 00:10:28 +0400

    H10

--- src/ru/progwards/java1/lessons/io1/Coder.java
+++ src/ru/progwards/java1/lessons/io1/Coder.java
@@ -5,27 +5,31 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Coder {
-    public static void codeFile(String inFileName, String outFileName, char[] code, String logName) throws IOException {
-        FileWriter logFile = new FileWriter(logName, true);
+    public static void codeFile(String inFileName, String outFileName, char[] code, String logName) {
         try {
-            FileReader reader = new FileReader(inFileName);
-            FileWriter writer = new FileWriter(outFileName, true);
-
+            FileWriter logFile = new FileWriter(logName, true);
             try {
-                int symbol = reader.read();
-                while (symbol != -1) {
-                    writer.write(code[symbol]);
-                    symbol = reader.read();
-                }
+                FileReader reader = new FileReader(inFileName);
+                FileWriter writer = new FileWriter(outFileName, true);
 
+                try {
+                    int symbol = reader.read();
+                    while (symbol != -1) {
+                        writer.write(code[symbol]);
+                        symbol = reader.read();
+                    }
+
+                } finally {
+                    reader.close();
+                    writer.close();
+                }
+            } catch (Exception e) {
+                logFile.write(e.getMessage() + "\n");
             } finally {
-                reader.close();
-                writer.close();
+                logFile.close();
             }
-        } catch (Exception e) {
-            logFile.write(e.getMessage() + "\n");
-        } finally {
-            logFile.close();
+        } catch (IOException e) {
+            //
         }
     }
 }
