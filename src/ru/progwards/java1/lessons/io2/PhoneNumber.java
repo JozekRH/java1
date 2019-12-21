20a580204a700dd65312afcb14b2d0c28ae1cc04
Артем <art-dikov@yandex.ru>
2019-12-21 21:39:28 +0400

    H11 - method PhoneNumber.format(String phone) can throw Exception

--- src/ru/progwards/java1/lessons/io2/PhoneNumber.java
+++ src/ru/progwards/java1/lessons/io2/PhoneNumber.java
@@ -3,17 +3,19 @@ package ru.progwards.java1.lessons.io2;
 public class PhoneNumber {
 
     public static void main(String[] args) {
-        System.out.println(format("7(467)7378478"));
+        //System.out.println(format("7(467)7378478"));
     }
 
-    public static String format(String phone) {
+    public static String format(String phone) throws Exception {
         StringBuilder sb = new StringBuilder(phone.replaceAll("[^0-9]", ""));
         if(sb.length() == 10) {
             sb.insert(0, "7");
-        } else {
+        } else if(sb.length() == 11) {
             if(sb.charAt(0) != '7') {
                 sb.replace(0, 1, "7");
             }
+        } else {
+            throw new Exception();
         }
         sb.insert(0, "+").insert(2, "(").insert(6, ")").insert(10, "-");
         return sb.toString();
