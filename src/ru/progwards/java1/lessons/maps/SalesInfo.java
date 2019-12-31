4c4da70d7abb074721036abaacec2c59a615809a
Артем <art-dikov@yandex.ru>
2019-12-31 14:48:31 +0400

    H15 done

--- src/ru/progwards/java1/lessons/maps/SalesInfo.java
+++ src/ru/progwards/java1/lessons/maps/SalesInfo.java
@@ -1,44 +1,88 @@
 package ru.progwards.java1.lessons.maps;
 
+import org.glassfish.grizzly.utils.Pair;
+
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class SalesInfo {
 
-    private static String orders = "";
+    private static String orders;
 
     public static int loadOrders(String fileName) {
+        StringBuilder stringBuilder = new StringBuilder();
+        int linesNumber = 0;
         try(FileReader fileReader = new FileReader(fileName);
             Scanner sc = new Scanner(fileReader)) {
-            StringBuilder sb = new StringBuilder();
-            int linesNumber = 0;
             while (sc.hasNextLine()) {
-                String str = sc.nextLine();
+                String str = sc.nextLine().trim();
                 String[] fieldsInLine = str.split("\\s*,\\s*");
-                //System.out.println(Arrays.toString(fieldsInLine));
                 if(fieldsInLine.length != 4 || !fieldsInLine[2].matches("\\d+")
                         || !fieldsInLine[3].matches("\\d+(.\\d+)?")) {
                     continue;
                 }
-                //System.out.println(str);
-                sb.append(fieldsInLine[0]);
+                stringBuilder.append(str).append("\n");
                 linesNumber++;
             }
-            orders = sc.toString();
-            return linesNumber;
+            orders = stringBuilder.toString();
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return 0;
+
+        return linesNumber;
+    }
+
+    public static Map<String, Double> getGoods() {
+        Map<String, Double> map = new TreeMap<>();
+        try(Scanner sc = new Scanner(orders)) {
+            while (sc.hasNextLine()) {
+                String str = sc.nextLine();
+                String[] fieldsInLine = str.split("\\s*,\\s*");
+                String productName = fieldsInLine[1];
+                Double cost = Double.valueOf(fieldsInLine[3]);
+                if(map.containsKey(productName)) {
+                    map.put(productName, map.get(productName)+cost);
+                } else {
+                    map.put(productName, cost);
+                }
+            }
+
+        }
+
+        return  map;
+    }
+
+    public static Map<String, Pair<Double, Integer>> getCustomers() {
+        Map<String, Pair<Double, Integer>> map = new TreeMap<>();
+        try(Scanner sc = new Scanner(orders)) {
+            while (sc.hasNextLine()) {
+                String str = sc.nextLine();
+                String[] fieldsInLine = str.split("\\s*,\\s*");
+                String customerName = fieldsInLine[0];
+                Integer goodsNumber = Integer.valueOf(fieldsInLine[2]);
+                Double cost = Double.valueOf(fieldsInLine[3]);
+                if(map.containsKey(customerName)) {
+                    Pair<Double, Integer> customerData = map.get(customerName);
+                    customerData.setFirst(customerData.getFirst() + cost);
+                    customerData.setSecond(customerData.getSecond() + goodsNumber);
+                    map.put(customerName, customerData);
+                } else {
+                    map.put(customerName, new Pair<>(cost, goodsNumber));
+                }
+            }
+
+        }
+        return  map;
     }
 
     public static void main(String[] args) {
-        String fileName = "C:\\Users\\artyom\\Desktop\\Новая папка (2)\\studyJava\\progwards\\Java Базовый уровень\\15 Словари (map)\\sales1.txt";
+        String fileName = "D:\\sales.csv";
         System.out.println(orders);
         System.out.println(loadOrders(fileName));
         System.out.println(orders);
+        System.out.println(getGoods());
+        System.out.println(getCustomers());
     }
 }
