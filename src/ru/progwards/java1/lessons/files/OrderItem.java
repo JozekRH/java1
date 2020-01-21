f62cad89860b044e79d1f3a11b6973566dfb4515
Артем <art-dikov@yandex.ru>
2020-01-22 02:43:09 +0400

    H17, task 3, loadOrders done

--- src/ru/progwards/java1/lessons/files/OrderItem.java
+++ src/ru/progwards/java1/lessons/files/OrderItem.java
@@ -2,7 +2,7 @@ package ru.progwards.java1.lessons.files;
 
 public class OrderItem {
 
-    public String googsName; // наименование товара
+    public String goodsName; // наименование товара
     public int count; // количество
     public double price; // цена за единицу
 }
--- src/ru/progwards/java1/lessons/files/OrderProcessor.java
+++ src/ru/progwards/java1/lessons/files/OrderProcessor.java
@@ -1,31 +1,96 @@
 package ru.progwards.java1.lessons.files;
 
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.ZonedDateTime;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.time.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class OrderProcessor {
 
     private String startPath;
-    private Map<String, List<Order>> ordersByShops;
+    private int failedFileNumber;
+    private Map<String, List<Order>> ordersByShops; // key: shopId, value: ordersList
 
     public OrderProcessor(String startPath) {
         this.startPath = startPath;
+        failedFileNumber = 0;
     }
 
-//    загружает заказы за указанный диапазон дат, с start до finish, обе даты включительно.
+    //    загружает заказы за указанный диапазон дат, с start до finish, обе даты включительно.
 //    Если start == null, значит нет ограничения по дате слева, если finish == null,
 //    значит нет ограничения по дате справа, если shopId == null - грузим для всех магазинов
 //    При наличии хотя бы одной ошибке в формате файла, файл полностью игнорируется, т.е. Не поступает в обработку.
 //    Метод возвращает количество файлов с ошибками. При этом, если в классе содержалась информация, ее надо удалить
     public int loadOrders(LocalDate start, LocalDate finish, String shopId) {
+
         if (start == null)
-            start = LocalDate.of(1970,1,1);
+            start = LocalDate.of(1970, 1, 1);
         if (finish == null)
             finish = LocalDate.now();
-        //
-        return 0;
+        if (shopId == null)
+            shopId = "";
+        PathMatcher pathMatcher = FileSystems.getDefault().getPathMatcher("glob:**/" + shopId + "*.txt");
+        try {
+            Files.walkFileTree(Paths.get(startPath), new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    String fileName = file.getFileName().toString();
+                    String[] idsArr = fileName.split("-");
+                    Order order = new Order();
+                    order.shopId = idsArr[0];
+                    order.orderId = idsArr[1];
+                    order.customerId = idsArr[2];
+                    order.datetime = LocalDateTime.ofInstant(Files.getLastModifiedTime(file).toInstant(), ZoneId.systemDefault());
+                    order.items = new ArrayList<>();
+                    order.sum = 0;
+                    List<String> goods = Files.readAllLines(file);
+                    for (String goodStr : goods
+                         ) {
+                        String[] strArr = goodStr.trim().split("\\s*,\\s*");
+
+                        if (strArr.length != 3) {
+                            failedFileNumber++;
+                            return FileVisitResult.CONTINUE;
+                        }
+
+                        int goodsCount = Integer.parseInt(strArr[1]);
+                        double oneGoodPrice = Double.parseDouble(strArr[2]);
+
+                        OrderItem orderItem = new OrderItem();
+                        orderItem.count = goodsCount;
+                        orderItem.price = oneGoodPrice;
+                        orderItem.goodsName = strArr[0];
+
+                        order.items.add(orderItem);
+                        order.sum += (goodsCount * oneGoodPrice);
+
+//                        for (char ch : strArr[1].toCharArray()
+//                             ) {
+//                            if (!Character.isDigit(ch))
+//                                return FileVisitResult.CONTINUE;
+//                        }
+//
+//                        int decimalSeparatorCounter = 0;
+//                        for (char ch : strArr[2].toCharArray()
+//                        ) {
+//                            if (!Character.isDigit(ch) || !String.valueOf(ch).equals("."))
+//                                return FileVisitResult.CONTINUE;
+//
+//                        }
+                    }
+
+                    ordersByShops.putIfAbsent(order.shopId, new ArrayList<>());
+                    ordersByShops.get(order.shopId).add(order);
+
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } catch (IOException e) {
+            //
+        }
+            return failedFileNumber;
+        }
     }
-}
