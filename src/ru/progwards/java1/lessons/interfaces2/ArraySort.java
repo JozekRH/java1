99479548ec8b58b7264f6a280684248da475b872
JozekRH <47397991+JozekRH@users.noreply.github.com>
2022-02-21 02:10:45 +0200

    Delete src/ru/progwards/java1/lessons/interfaces2 directory

--- src/ru/progwards/java1/lessons/interfaces2/ArraySort.java
+++
@@ -1,9 +0,0 @@
-package ru.progwards.java1.lessons.interfaces2;
-
-import java.util.Arrays;
-
-public class ArraySort {
-    public static void sort(Comparable<Number>[] a) {
-        Arrays.sort(a);
-    }
-}
